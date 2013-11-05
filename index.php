b60de2eca953940e2a7e1ac4d73e22e72567d9e8
breezety <breezegyj@gmail.com>
2013-11-05 23:14:33 +0800

    2013-11-05 test1

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
 <?php
 //单入口模式
-//test
-//tessdfsd1
-//123123123123
 error_reporting(0);
 @set_time_limit(240);
 @ini_set("memory_limit",'-1');
 require("./core/Conf/define.php");
 define('APP_DEBUG', true);
-define('APP_DEBUG', true);
-adfs;
-phpinfo();
 require(THINK_PATH."/ThinkPHP.php");
 $App = new App();
 $App->run();
